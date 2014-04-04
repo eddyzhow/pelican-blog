85bafc4907d87dfafbad5afbb924eec900d959da
eddyzhow <grit.ni@gmail.com>
2014-04-05 01:33:05 +0700

    Initial commit.

---
+++ content/how-to-batch-convert-images-in-ubuntu.md
@@ -0,0 +1,19 @@
+Title: How to batch convert images in Ubuntu 12.04
+Date: 2014-04-04 21:06
+Category: Tips
+Tags: ubuntu, imagemagick
+Slug: how-to-batch-convert-images-in-ubuntu
+Author: eddyzhow
+Summary:
+
+Install imagemagick :
+
+```
+sudo apt-get install libmagickwand-dev imagemagick
+```
+
+and then cd to your directory and run :
+
+```
+for file in *.jpg ; do convert “$file[144x]” “./thumbs/${file/%jpg/png}” ; done
+```
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'eddyzhow'
+SITENAME = u'eddyatlab'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'th'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
+
+# Social widget
+SOCIAL = (('facebook', 'http://www.facebook.com/eddyzhow'),
+          ('soundcloud', 'https://www.soundcloud.com/eddyzhow'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
